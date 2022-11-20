02006c698f9f0be938b266fada447e59466b5bf6
Arda Örkin <ardaorkin@gmail.com>
2022-11-20 20:51:15 +0300

    react query & react location

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  Link,
+  Outlet,
+  ReactLocation,
+  Router,
+  useMatch,
+} from "@tanstack/react-location";
 import { useEffect, useRef } from "react";
 import "./App.css";
 import { PokemonProvider, usePokemon } from "./store";
 
+const queryClient = new QueryClient();
+const location = new ReactLocation();
 const SearchBox = () => {
   const { search, setSearch } = usePokemon();
   const inputRef = useRef({} as HTMLInputElement);
@@ -15,6 +25,7 @@ const SearchBox = () => {
       disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
       invalid:border-pink-500 invalid:text-pink-600
       focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+      placeholder="Search"
       ref={inputRef}
       type={"text"}
       value={search}
@@ -28,32 +39,98 @@ const PokemonList = () => {
   return (
     <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 mt-3">
       {pokemon.map((p) => (
-        <li
-          key={p.id}
-          className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200"
-        >
-          <div className="flex-1 flex flex-col p-8">
-            <img
-              className="w-32 h-32 flex-shrink-0 mx-auto bg-black rounded-full"
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${p.id}.png`}
-              alt=""
-            />
-            <h3 className="mt-6 text-gray-900 text-sm font-medium">{p.name}</h3>
-          </div>
-        </li>
+        <Link key={p.id} to={`/pokemon/${p.id}`}>
+          <li className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200">
+            <div className="flex-1 flex flex-col p-8">
+              <img
+                className="w-32 h-32 flex-shrink-0 mx-auto bg-black rounded-full"
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${p.id}.png`}
+                alt=""
+              />
+              <h3 className="mt-6 text-gray-900 text-sm font-medium">
+                {p.name}
+              </h3>
+            </div>
+          </li>
+        </Link>
       ))}
     </ul>
   );
 };
 
-function App() {
+function PokemonDetail() {
+  const {
+    params: { id },
+  } = useMatch();
+  const { pokemon } = usePokemon();
+  const pokemonData = pokemon.find((p) => p.id === +id);
+  if (!pokemonData) {
+    return <div>No pokemon found</div>;
+  }
   return (
-    <PokemonProvider>
-      <div className="mx-auto max-w-3xl">
+    <div className="mt-3">
+      <Link to="/">
+        <h1 className="text-2xl font-bold mb-5">&lt; Home</h1>
+      </Link>
+      <div className="grid grid-cols-2">
+        <img
+          className="w-96 h-96 flex-shrink-0 mx-auto bg-black rounded-xl"
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonData.id}.png`}
+          alt=""
+        />
+        <div className="ml-3">
+          <h2 className="text-2xl font-bold">{pokemonData.name}</h2>
+          <div className="mt-3">
+            <h3 className="text-xl font-bold">Stats</h3>
+            <ul className="mt-3">
+              {[
+                "hp",
+                "attack",
+                "defense",
+                "special_attack",
+                "special_defense",
+                "speed",
+              ].map((stat) => (
+                <li key={stat} className="grid grid-cols-2">
+                  <span className="font-bold">{stat}</span>
+                  <span>{pokemonData[stat as keyof typeof pokemonData]}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const routes = [
+  {
+    path: "/",
+    element: (
+      <>
         <SearchBox />
         <PokemonList />
-      </div>
-    </PokemonProvider>
+      </>
+    ),
+  },
+  {
+    path: "/pokemon/:id",
+    element: <PokemonDetail />,
+  },
+];
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <PokemonProvider>
+        <Router location={location} routes={routes}>
+          <div className="mx-auto max-w-3xl">
+            <Outlet />
+          </div>
+        </Router>
+      </PokemonProvider>
+    </QueryClientProvider>
   );
 }
 
--- src/store.tsx
+++ src/store.tsx
@@ -1,16 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
 import React, {
   createContext,
   useCallback,
   useContext,
-  useEffect,
   useMemo,
   useReducer,
-  useState,
 } from "react";
 
 interface Pokemon {
   id: number;
   name: string;
+  type: string[];
+  hp: number;
+  attack: number;
+  defense: number;
+  special_attack: number;
+  special_defense: number;
   speed: number;
 }
 
@@ -19,34 +24,29 @@ function usePokemonSource(): {
   search: string;
   setSearch: (search: string) => void;
 } {
+  const { data: pokemon } = useQuery<Pokemon[]>(
+    ["pokemon"],
+    () => fetch("/pokemon.json").then((res) => res.json()),
+    {
+      initialData: [],
+    }
+  );
   type PokemonState = {
-    pokemon: Pokemon[];
     search: string;
   };
 
-  type PokemonAction =
-    | { type: "setPokemon"; payload: Pokemon[] }
-    | { type: "setSearch"; payload: string };
-  const [{ pokemon, search }, dispatch] = useReducer(
+  type PokemonAction = { type: "setSearch"; payload: string };
+  const [{ search }, dispatch] = useReducer(
     (state: PokemonState, action: PokemonAction) => {
       switch (action.type) {
-        case "setPokemon":
-          return { ...state, pokemon: action.payload };
         case "setSearch":
           return { ...state, search: action.payload };
       }
     },
     {
-      pokemon: [],
       search: "",
     }
   );
-
-  useEffect(() => {
-    fetch("/pokemon.json")
-      .then((response) => response.json())
-      .then((data) => dispatch({ type: "setPokemon", payload: data }));
-  }, []);
 
   const setSearch = useCallback((search: string) => {
     dispatch({ type: "setSearch", payload: search });
